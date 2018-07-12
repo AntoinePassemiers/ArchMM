fa18048df2d06384ec5f2e6a0b2e06461c9345be
AntoinePassemiers <apassemi@ulb.ac.be>
2018-07-12 13:35:19 +0200

    Google style docstrings

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -30,12 +30,14 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
+extensions = [
+    'sphinx.ext.autodoc',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages']
+    'sphinx.ext.githubpages',
+    'sphinxcontrib.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -156,6 +158,3 @@ texinfo_documents = [
      author, 'archmm', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
