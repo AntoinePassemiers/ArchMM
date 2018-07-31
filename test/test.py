e4945add14bfec862e8fb3d6a0b3dacfbc707c3a
AntoinePassemiers <apassemi@ulb.ac.be>
2018-07-31 11:44:21 +0200

    Remove old test folder

--- test/test.py
+++ test/test.py
@@ -8,6 +8,17 @@ from numpy.testing import assert_almost_equal
 from archmm.hmm import GHMM
 
 
+def find_nearest(arr, value, axis=1):
+    return np.linalg.norm(np.abs(arr - value), axis=axis).argmin()
+
+
+def assert_array_almost_equal_by_permuting(A, B, axis=0, **kwargs):
+    permuted_A = np.empty(A.shape)
+    for i in range(len(B)):
+        permuted_A[i] = A[find_nearest(A, B[i], axis=1-axis)]
+    assert_array_almost_equal(permuted_A, B, **kwargs)
+
+
 def test_gmm_multi_sequence():
     hmm = GHMM(3, arch='ergodic')
     X1 = np.random.rand(150, 10)
