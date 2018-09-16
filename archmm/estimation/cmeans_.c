3dd9db341b09e0d3d6d98f3c72506c53b57bd625
AntoinePassemiers <apassemi@ulb.ac.be>
2018-09-16 08:42:26 +0200

    Remove loop initial declarations

--- archmm/estimation/cmeans_.c
+++ archmm/estimation/cmeans_.c
@@ -11,32 +11,32 @@ void fuzzyCMeans(double** X,
                  int fuzzy_coef) {
     int n_iter = 0;
     while (n_iter++ < max_n_iter) {
-
-        for (int j = 0; j < n_clusters; j++) {
+        int i, j, k, l;
+        for (j = 0; j < n_clusters; j++) {
             memset((void*) centroids[j], 0x00, n_features * sizeof(double));
             double denom = 0.0;            
-            for (int i = 0; i < n_samples; i++) {
-                for (int l = 0; l < n_features; l++) {
+            for (i = 0; i < n_samples; i++) {
+                for (l = 0; l < n_features; l++) {
                     centroids[j][l] += U[i][j] * X[i][l];
                 }
                 denom += U[i][j];
             }
-            for (int l = 0; l < n_features; l++) {
+            for (l = 0; l < n_features; l++) {
                 centroids[j][l] /= denom;
             }
         }
         int n_unchanged = 0;
-        for (int j = 0; j < n_clusters; j++) {
-            for (int i = 0; i < n_samples; i++) {
+        for (j = 0; j < n_clusters; j++) {
+            for (i = 0; i < n_samples; i++) {
                 double denomnum = 0.0;
-                for (int l = 0; l < n_features; l++) {
+                for (l = 0; l < n_features; l++) {
                     denomnum += pow(X[i][l] - centroids[j][l], 2);
                 }
                 denomnum = sqrt(denomnum);
                 double denom = 0.0;
-                for (int k = 0; k < n_clusters; k++) {
+                for (k = 0; k < n_clusters; k++) {
                     double denomdenom = 0.0;
-                    for (int l = 0; l < n_features; l++) {
+                    for (l = 0; l < n_features; l++) {
                         denomdenom += pow(X[i][l] - centroids[k][l], 2);
                     }
                     denomdenom = sqrt(denomdenom);
